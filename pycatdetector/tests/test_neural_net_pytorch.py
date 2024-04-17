a8e24cd4c33c198d140cd0751b3cac74a39ee945
Olaf Reitmaier <olafrv@gmail.com>
2024-04-17 23:34:27 +0200

    Test renamed

---
+++ pycatdetector/tests/test_neural_net_pytorch.py
@@ -0,0 +1,18 @@
+from pycatdetector.NeuralNetPyTorch import NeuralNetPyTorch
+import os
+
+
+def test_main():
+    nn = NeuralNetPyTorch()
+    assert 'cat' in nn.get_classes()
+    print()
+    dirname = os.path.join(os.curdir, 'pycatdetector', 'tests', 'images')
+    for filename in os.listdir(dirname):
+        fullpath = os.path.join(dirname, filename)
+        scored_labels = nn.get_scored_labels(0.5, nn.analyze(fullpath))
+        print(fullpath + " => " + repr(scored_labels))
+        labels = [label['label'] for label in scored_labels]
+        if len(labels) == 0:
+            assert False
+        else:
+            assert 'cat' in labels
