83c913d27bea6a0bbf2a88041b609a0410e62c7a
Olaf Reitmaier <olafrv@gmail.com>
2024-04-20 15:30:21 +0200

    Docstring

--- pycatdetector/NeuralNetPyTorch.py
+++ pycatdetector/NeuralNetPyTorch.py
@@ -5,28 +5,54 @@ import PIL
 from torchvision.io import read_image
 from torchvision.transforms import ToTensor, ToPILImage
 from torchvision.utils import draw_bounding_boxes
-from .AbstractNeuralNet import AbstractNeuralNet
-
-
-# https://pytorch.org/vision/main/models.html
-# https://pytorch.org/vision/stable/_modules/torchvision/models/detection/ssdlite.html
-# https://pytorch.org/vision/stable/_modules/torchvision/models/detection/faster_rcnn.html
 from torchvision.models.detection import \
     fasterrcnn_mobilenet_v3_large_fpn, \
     fasterrcnn_mobilenet_v3_large_320_fpn, \
     FasterRCNN_MobileNet_V3_Large_320_FPN_Weights, \
     FasterRCNN_MobileNet_V3_Large_FPN_Weights
+from .AbstractNeuralNet import AbstractNeuralNet
 
 
 class NeuralNetPyTorch(AbstractNeuralNet):
-    weights = None
-    model = None
-    preprocess = None
+    """
+    NeuralNetPyTorch is a class that represents a neural network
+    model implemented using PyTorch, references:
+    - https://pytorch.org/vision/main/models.html
+    - https://pytorch.org/vision/stable/_modules/torchvision/models/detection/ssdlite.html  # noqa
+    - https://pytorch.org/vision/stable/_modules/torchvision/models/detection/faster_rcnn.html  # noqa
+
+    Args:
+        model_name (str): The name of the model to use.
+        min_score (float, optional): The minimum score threshold for 
+                                     object detection. Defaults to 0.7.
+
+    Raises:
+        ValueError: If an invalid model_name is provided.
+
+    Attributes:
+        weights: The weights of the model.
+        model: The PyTorch model.
+        preprocess: The preprocessing steps for the input image.
+
+    """
 
     def __init__(self, model_name, min_score=0.7):
+        """
+        Initializes a NeuralNetPyTorch object.
+
+        Args:
+            model_name (str): The name of the model to use.
+            min_score (float, optional): The minimum score threshold for
+                                         object detection. Defaults to 0.7.
+
+        Raises:
+            ValueError: If an invalid model_name is provided.
+
+        """
 
         if model_name == "FasterRCNN_MobileNet_V3_Large_FPN":
-            self.weights = FasterRCNN_MobileNet_V3_Large_FPN_Weights.DEFAULT
+            self.weights = \
+                FasterRCNN_MobileNet_V3_Large_FPN_Weights.DEFAULT
             self.model = fasterrcnn_mobilenet_v3_large_fpn(
                 weights=self.weights, box_score_thresh=min_score)
         elif model_name == "FasterRCNN_MobileNet_V3_Large_320_FPN":
@@ -40,7 +66,23 @@ class NeuralNetPyTorch(AbstractNeuralNet):
         self.model.eval()
         self.preprocess = self.weights.transforms()
 
-    def analyze(self, image):
+    def analyze(self, image) -> dict:
+        """
+        Analyzes an image using the neural network model.
+
+        Args:
+            image (str or PIL.Image.Image or numpy.ndarray):
+                The input image to analyze.
+
+        Returns:
+            dict: A dictionary containing the analysis results,
+                  including the image, labels, scores, and boxes.
+
+        Raises:
+            FileNotFoundError: If the input image file is not found.
+
+        """
+
         img = None
         if isinstance(image, str):
             if os.path.exists(image):
@@ -64,10 +106,31 @@ class NeuralNetPyTorch(AbstractNeuralNet):
             "boxes": prediction["boxes"]
         }
 
-    def get_classes(self):
+    def get_classes(self) -> list:
+        """
+        Returns the classes/categories used by the model.
+
+        Returns:
+            list: A list of class/category names.
+
+        """
+
         return self.weights.meta["categories"]
 
-    def get_scored_labels(self, result, min_score=-1):
+    def get_scored_labels(self, result, min_score=-1) -> list:
+        """
+        Filters the labels based on the minimum score threshold.
+
+        Args:
+            result (dict): The analysis result dictionary.
+            min_score (float, optional): The minimum score threshold.
+                                         Defaults to -1.
+
+        Returns:
+            list: A list of scored labels.
+
+        """
+
         labels = result["labels"]
         scores = result["scores"].detach().numpy()
         boxes = result["boxes"].detach().numpy()
@@ -82,6 +145,17 @@ class NeuralNetPyTorch(AbstractNeuralNet):
         return scored_labels
 
     def plot(self, result) -> PIL.Image.Image:
+        """
+        Plots the bounding boxes on the image.
+
+        Args:
+            result (dict): The analysis result dictionary.
+
+        Returns:
+            PIL.Image.Image: The image with bounding boxes plotted.
+
+        """
+
         img = result["image"]
         labels = result["labels"]
         boxes = result["boxes"]
