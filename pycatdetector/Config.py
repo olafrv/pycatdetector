49a2e5f3ce8cf93dfa7a15eae1b0ab9aa22945ce
Olaf Reitmaier <olafrv@gmail.com>
2024-04-20 09:35:47 +0200

    Docstring and readability

--- pycatdetector/Config.py
+++ pycatdetector/Config.py
@@ -3,20 +3,49 @@ import yaml
 
 
 class Config:
-    config = None
-    config_flat = None
+    """
+    The Config class represents a configuration object that loads
+    and provides access to configuration settings.
+
+    Attributes:
+        CONFIG (dict): Loaded configuration settings (i.e. key1.key2).
+        CONFIG_FLAT (dict): Flattened configuration settings (i.e. key1_key2).
+
+    """
 
     def __init__(self, config_file='config.yaml'):
-        with open(config_file, 'r') as stream:
-            self.config = yaml.safe_load(stream)
-        self.config_flat = self.flatten(self.config)
+        """
+        Initializes a new instance of the Config class.
 
-    def flatten(cls, config={}, separator="_"):
+        Args:
+            config_file (str): The path to the configuration file.
+                               Default is 'config.yaml'.
+
+        """
+        with open(config_file, 'r') as stream:
+            self.CONFIG = yaml.safe_load(stream)
+
+        self.CONFIG_FLAT = self.flatten(self.CONFIG)
+
+    def flatten(self, config={}, separator="_"):
+        """
+        Flattens a nested dictionary into a flat dictionary.
+
+        Args:
+            config (dict): The nested dictionary to be flattened. 
+                           Default is an empty dictionary.
+            separator (str): The separator to be used for the flattened keys.
+                             Default is '_'.
+
+        Returns:
+            dict: The flattened dictionary.
+
+        """
         flattened_config = {}
         for key in config:
             value = config[key]
             if isinstance(value, dict):
-                recursive_config = cls.flatten(value)
+                recursive_config = self.flatten(value)
                 for rkey in recursive_config:
                     rvalue = recursive_config[rkey]
                     flattened_config[key + separator + str(rkey)] = rvalue
@@ -25,34 +54,94 @@ class Config:
         return flattened_config
 
     def get_regex(self, regex):
+        """
+        Retrieves configuration settings that match a given regular expression.
+
+        Args:
+            regex (str): Regexp pattern to match against configuration keys.
+
+        Returns:
+            dict: A dictionary containing the matching configuration settings.
+
+        """
         config = {}
-        for key in self.config_flat:
+        for key in self.CONFIG_FLAT:
             if re.match(regex, key):
-                config[key] = self.config_flat[key]
+                config[key] = self.CONFIG_FLAT[key]
         return config
 
     def get_prefix(self, prefix="", ltrim=True):
-        config = {}
-        for key in self.config_flat:
+        """
+        Retrieves configuration settings that have a given prefix.
+
+        Args:
+            prefix (str): The prefix to match against the configuration keys.
+                          Default is an empty string, which matches all keys.
+            ltrim (bool): Specifies whether to remove the prefix from the
+                          returned keys. Default is True.
+
+        Returns:
+            dict: A dictionary containing the matching configuration settings.
+
+        """
+        filtered_config = {}
+        for key in self.CONFIG_FLAT:
             if re.match("^"+prefix+"_", key):
                 if ltrim:
                     ltrim_key = str(key).replace(prefix+"_", '', 1)
-                    config[ltrim_key] = self.config_flat[key]
+                    filtered_config[ltrim_key] = self.CONFIG_FLAT[key]
                 else:
-                    config[key] = self.config[key]
-        return config
+                    filtered_config[key] = self.CONFIG[key]
+        return filtered_config
 
     def get(self, name) -> dict:
-        return self.config_flat[name]
+        """
+        Retrieves a specific configuration setting by its key.
+
+        Args:
+            name (str): The key of the configuration setting to retrieve.
+
+        Returns:
+            dict: The configuration setting.
+
+        """
+        return self.CONFIG_FLAT[name]
 
     def get_assoc(self, key) -> dict:
-        return self.config[key]
+        """
+        Retrieves a nested configuration setting by its key.
+
+        Args:
+            key (str): The key of the nested configuration setting to retrieve.
+
+        Returns:
+            dict: The nested configuration setting.
+
+        """
+        return self.CONFIG[key]
 
     def camel_to_snake(s):
+        """
+        Converts a camel case string to snake case.
+
+        Args:
+            s (str): The camel case string to convert.
+
+        Returns:
+            str: The snake case string.
+
+        """
         return ''.join([
                         '_' +
                         c.lower() if c.isupper() else c for c in s
                       ]).lstrip('_')
 
     def __str__(self) -> str:
-        return yaml.dump(self.config)
+        """
+        Returns a string representation of the Config instance.
+
+        Returns:
+            str: The string representation of the Config instance.
+
+        """
+        return yaml.dump(self.CONFIG)
