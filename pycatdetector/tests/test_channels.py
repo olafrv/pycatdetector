7998f7ba1f2874c13cd9198b2c83f649c70602ec
Olaf Reitmaier <olafrv@gmail.com>
2025-07-19 21:57:26 +0200

    Fixed bug in tests due to function changes

--- pycatdetector/tests/test_channels.py
+++ pycatdetector/tests/test_channels.py
@@ -10,16 +10,27 @@ def test_main(caplog):
     config = Config()
     channel_name = "ha_google_speak"
     if config.get("notifiers_" + channel_name + "_enabled"):
+        print(f"Channel '{channel_name}' enabled, running test...")
         filtered_config = config.get_prefix("notifiers_" + channel_name)
         channel = HaGoogleSpeak(filtered_config)
-        assert channel.notify("Esto es una prueba de sonido más larga.")
+        assert channel.notify(custom_content={
+            "message": "Esto es una prueba de sonido más larga."
+        })
     else:
-        print("Channel not enabled, test skipped.")
+        print(f"Channel '{channel_name}' not enabled, test skipped.")
         assert True
 
     print()
     channel_name = "discord_webhook"
     if config.get("notifiers_" + channel_name + "_enabled"):
+        print(f"Channel '{channel_name}' enabled, running test...")
         filtered_config = config.get_prefix("notifiers_" + channel_name)
         channel = DiscordWebhook(filtered_config)
-        assert channel.notify("Esto es una prueba a través de chat.")
+        assert channel.notify(
+            custom_content={
+                "message": "Esto es una prueba a través de chat."
+            }
+        )
+    else:
+        print(f"Channel '{channel_name}' not enabled, test skipped.")
+        assert True
