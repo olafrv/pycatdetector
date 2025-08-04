d37c62b57bd05889e63f7579d82b6e79b84f7d7c
Olaf Reitmaier <olafrv@gmail.com>
2025-08-04 19:33:49 +0200

    space

--- pycatdetector/channels/AbstractChannel.py
+++ pycatdetector/channels/AbstractChannel.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Optional, Dict, Any
+
 class AbstractChannel(ABC):
     """
     An abstract base class for different channel implementations.
